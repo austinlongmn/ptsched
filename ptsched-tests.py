261bb930328e7a66a068a1231740012729463634
Austin <137583592+austinlongmn@users.noreply.github.com>
2024-08-17 08:56:56 -0700

    Added tests for errors

--- ptsched-tests.py
+++ ptsched-tests.py
@@ -9,31 +9,48 @@ import ptsched
 
 class Test_ptsched(unittest.TestCase):
 	def test_parser(self):
-		for (input_filename, expected_output_filename) in self.get_input_and_expected_outputs("ast.json"):
+		for (input_filename, expected_output_filename) in self.get_input_and_expected_outputs("ast.json", "24"):
 			with open(input_filename) as input_file, open(expected_output_filename) as expected_output_file:
-				self.assertEqual(json.loads(json.dumps(ptsched.parse_file(input_file), default=str)), json.load(expected_output_file), msg="File %s failed the AST test." % input_filename)
+				self.assertEqual(
+					json.loads(json.dumps(ptsched.parse_file(input_file), default=str)),
+					json.load(expected_output_file), msg="File %s failed the AST test." % input_filename
+				)
 	
 	def test_final_results(self):
-		for (input_filename, expected_output_filename) in self.get_input_and_expected_outputs("out.txt"):
+		for (input_filename, expected_output_filename) in self.get_input_and_expected_outputs("out.txt", "24"):
 			with open(expected_output_filename) as expected_output_file:
-				recovery_stdout = sys.stdout
-				strIO = io.StringIO()
 				ptsched.parse(["-o", "temp_test_result", input_filename])
 				with open("temp_test_result") as result:
 					self.assertEqual(result.read(), expected_output_file.read(), msg="File %s failed the final results test." % input_filename)
 		os.remove("temp_test_result")
 
+	def test_throws(self):
+		for (input_filename, expected_output_filename) in self.get_input_and_expected_outputs("error.txt", "throwing"):
+			with open(expected_output_filename) as expected_error_file, open(input_filename) as input_file:
+				error_details = expected_error_file.read()
+				split = error_details.splitlines()
+				error_type = split[0]
+				error_message = split[1]
+				if error_type == "PTSchedParseException":
+					error = ptsched.PTSchedParseException
+				elif error_type == "PTSchedValidationException":
+					error = ptsched.PTSchedValidationException
+				
+				with self.assertRaises(error) as cm:
+					ptsched.parse_file(input_file)
 
-	def get_input_and_expected_outputs(self, suffix):
+				self.assertEqual(str(cm.exception), str(error(error_message)))
+
+	def get_input_and_expected_outputs(self, suffix, criteria):
 		result = []
-		for (dir, _, files) in os.walk("test-workspace"):
-			if "expected-output" in dir or "out" in dir:
+		for (dir, _, files) in os.walk("test-workspace/input"):
+			if "expected-output" in dir or "out" in dir or not criteria in dir:
 				continue
 			for file in files:
 				if not "ptsched" in file:
 					continue
 				input_filename = dir + "/" + file
-				test_directory = "%s%s%s%s%s" % (dir.replace("test-workspace/", "test-workspace/expected-output/"), "/", file, "/", suffix)
+				test_directory = "%s%s%s%s%s" % (dir.replace("input/", "expected-output/"), "/", file, "/", suffix)
 				result.append((input_filename, test_directory))
 		return result
 
